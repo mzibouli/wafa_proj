6115ee950703a36b676685a1f648719685ed7c13
Mohamed Zibouli <ah98890@ens.etsmtl.ca>
2016-06-18 13:40:13 -0400

    rename some classes and add setup squelette angular controllers

--- Wafa/gestionStock/src/main/java/com/gestionStock/controllers/ProduitController.java
+++ Wafa/gestionStock/src/main/java/com/gestionStock/controllers/ProduitController.java
@@ -1,10 +1,21 @@
 package com.gestionStock.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gestionStock.model.Produit;
+import com.gestionStock.repositoryServiceImpl.ProduitRepositoryServiceImpl;
 
 @RestController
 @RequestMapping("/produit")
 public class ProduitController {
-
+	@RequestMapping(value = "/tousLesProduits", method = RequestMethod.GET)
+	public Iterable<Produit> tousLesProduits() {
+		return produitRepositoryServiceImpl.findAll();
+	}
+	
+	@Autowired
+	ProduitRepositoryServiceImpl produitRepositoryServiceImpl;
 }
--- Wafa/gestionStock/src/main/resources/static/js/app.js
+++ Wafa/gestionStock/src/main/resources/static/js/app.js
@@ -1,8 +1,22 @@
 var app = angular.module("gestionStock", []);
 
-app
-		.controller(
-				"MainController",
-				function($scope, $http) {
-					
+app.controller("MainController", function($scope, $http) {
+	$scope.getAllProduits = function() {
+		$http.get('produit/tousLesProduits').success(function(data) {
+			$scope.produits = data;
+			alert($scope.produits);
 		});
+	}
+});
+
+app.controller("ProduitController", function($scope, $http) {
+
+});
+
+app.controller("ClientController", function($scope, $http) {
+
+});
+
+app.controller("FournisseurController", function($scope, $http) {
+
+});
